67641e059e8303cb51b4daf24a9ff1ef79c1e6f5
rogcomfox <joeg.11021996@gmail.com>
2020-05-06 08:43:19 +0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Joe G on 06/05/20.
+//  Copyright © 2020 Nusantarian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
